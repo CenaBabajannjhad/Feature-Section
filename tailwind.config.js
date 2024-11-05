1dc5adecd5ed4ff4d22f6e98b5ef4e5b824dc2c8
CenaBabajannjhad <cenababajannjhad@gmail.com>
2024-11-05 21:28:43 +0330

    finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,31 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      "semibold" : "outfit-semibold",
+      "regular" : "outfit-reg"
+    },
+
+
+    extend: {
+      backgroundImage: {
+        "main-background" : "url('../../assets/svg/Background_image.svg')"
+      },
+      colors: {
+        "dark-charcoal" : "#121826",
+        "gunmetal" : "#212936",
+        "platinum" : "#E5E7EB",
+        "cadet-grey" : "#9DA3AE",
+        "light-periwinkle" : "#DEE9FC",
+        "misty-rose" : "#FBE5E6",
+        "light-apricot" : "#FCEED8"
+      },
+      fontSize: {
+        "heading" : "2.5rem",
+        "small-heading" : "1.25rem",
+        "body-text" : "1rem"
+      },
+    },
   },
   plugins: [],
 }
-
